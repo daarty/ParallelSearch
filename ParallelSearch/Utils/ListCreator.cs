08b2b549bfe357cc7335e33f91224c7b827bcaea
daarty <daarty@freenet.de>
2020-03-21 17:48:17 +0100

    Fix the ListBox style so that items are no longer selectable.

--- ParallelSearch/Utils/ListCreator.cs
+++ ParallelSearch/Utils/ListCreator.cs
@@ -7,15 +7,28 @@ namespace ParallelSearch.Utils
     /// </summary>
     public class ListCreator : IListCreator
     {
+        private static readonly List<string> Characters = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
         /// </inheritdoc>
         public List<string> CreateWordList()
         {
             var list = new List<string>();
+            var indices = new List<int>();
 
-            // TODO implement the creation
-            list.Add("A");
-            list.Add("B");
-            list.Add("C");
+            foreach (var first in Characters)
+            {
+                foreach (var second in Characters)
+                {
+                    foreach (var third in Characters)
+                    {
+                        foreach (var fourth in Characters)
+                        {
+                            indices.Add(list.Count);
+                            list.Add($"{first}{second}{third}{fourth}");
+                        }
+                    }
+                }
+            }
 
             return list;
         }
